7b3495ee11b768e1e36a617293888bfc7346d93e
Muhammad Haider Sheikh <Haidersheikh243@gmail.com>
2024-09-07 11:38:21 +0500

    logut-button

--- src/app/doctor-dashboard/page.tsx
+++ src/app/doctor-dashboard/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import DoctorDashboard from '@/components/doctor-dashboard/dashboard'
+import Header from '@/components/logout-button/logout'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
     <div>
+        <Header />
         <DoctorDashboard />
     </div>
   )
--- src/app/patient-dashboard/page.tsx
+++ src/app/patient-dashboard/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Dashboard from '@/components/patient-dashboard/dashboard'
+import Header from '@/components/logout-button/logout'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
     <div>
+        <Header />
         <Dashboard />
     </div>
   )
