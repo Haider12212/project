2b3e73f02df3bf3d384ecda86fae56b2ca71cf39
Zohaib <101661680+zabi1647@users.noreply.github.com>
2024-09-07 14:14:22 +0500

    added protaction to pages

--- src/app/admin-dashboard/page.tsx
+++ src/app/admin-dashboard/page.tsx
@@ -1,16 +1,45 @@
-import AdminDashboard from '@/components/admin-dashboard/dashboard'
-import React from 'react'
-import Header from '@/components/logout-button/logout'
+'use client';
+
+import AdminDashboard from '@/components/admin-dashboard/dashboard';
+import React, { useEffect, useState } from 'react';
+import Header from '@/components/logout-button/logout';
+import { useSession } from 'next-auth/react';
 
 type Props = {}
 
-const page = (props: Props) => {
+const AdminPage = (props: Props) => {
+  const { data: session, status } = useSession();
+  const [isAuthorized, setIsAuthorized] = useState(false);
+
+  useEffect(() => {
+    if (session && session.user?.userType === 'admin') {
+      setIsAuthorized(true);
+    } else {
+      setIsAuthorized(false);
+    }
+  }, [session]);
+
+  if (status === 'loading') {
+    return <p>Loading...</p>;
+  }
+
+  if (!isAuthorized) {
+    return (
+      <div>
+        <Header />
+        <p className="text-red-500 text-center mt-6">
+          You do not have the privilege to access this page.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div>
-        <Header/>
-        <AdminDashboard numDoctors={0} numAppointments={0} numPatients={0}/>
+      <Header />
+      <AdminDashboard numDoctors={0} numAppointments={0} numPatients={0} />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default AdminPage;
--- src/app/doctor-dashboard/page.tsx
+++ src/app/doctor-dashboard/page.tsx
@@ -1,16 +1,45 @@
-import React from 'react'
-import DoctorDashboard from '@/components/doctor-dashboard/dashboard'
-import Header from '@/components/logout-button/logout'
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import DoctorDashboard from '@/components/doctor-dashboard/dashboard';
+import Header from '@/components/logout-button/logout';
+import { useSession } from 'next-auth/react';
 
 type Props = {}
 
-const page = (props: Props) => {
+const DoctorPage = (props: Props) => {
+  const { data: session, status } = useSession();
+  const [isAuthorized, setIsAuthorized] = useState(false);
+
+  useEffect(() => {
+    if (session && session.user?.userType === 'doctor') {
+      setIsAuthorized(true);
+    } else {
+      setIsAuthorized(false);
+    }
+  }, [session]);
+
+  if (status === 'loading') {
+    return <p>Loading...</p>;
+  }
+
+  if (!isAuthorized) {
+    return (
+      <div>
+        <Header />
+        <p className="text-red-500 text-center mt-6">
+          You do not have the privilege to access this page.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div>
-        <Header />
-        <DoctorDashboard />
+      <Header />
+      <DoctorDashboard />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default DoctorPage;
